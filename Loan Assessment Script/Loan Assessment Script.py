ad33ca4f6b876f28226e3be729b5db4f5f128888
Ramkumar Rajagopalan <ramrajag6@gmail.com>
2018-11-04 14:48:58 +0530

    code

--- Loan Assessment Script/Loan Assessment Script.py
+++ Loan Assessment Script/Loan Assessment Script.py
@@ -1,7 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Loan Assessment
 """
 
+import numpy as np
+import pandas as pd
+
+train = pd.read_csv("loan_train.csv")
+print(train.head(5))
+print(train.describe())
+
+predictors = train.columns.values
+print(predictors)
+predictors = np.delete(predictors,0)
+
+
+train.count()
+
+
+
+train_dummy = pd.get_dummies(train,dummy_na=True)
+
+train_dummy.count()
